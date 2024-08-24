3156b78e22f4dca9cbcc64ee6c68a50dbc01aebc
Kelly Xiong Chen <kellyxiongc@gmail.com>
2024-08-24 09:47:00 -0400

    WIP: 3 Different Fetches and More

--- pokemon-battlefield/src/components/PokemonSelection.jsx
+++ pokemon-battlefield/src/components/PokemonSelection.jsx
@@ -10,40 +10,82 @@ const PokemonSelection = ({ setCurrentOption }) => {
     const [nameEntry, settingName] = useState('');
     const [typeEntry, settingType] = useState('');
     const [genEntry, settingGen] = useState('');
-    // console.log(userEntry);
+    // console.log('Hello');
+    // console.log(nameEntry, typeEntry, genEntry);
     const [error, setError] = useState();
     // use the context given the state of PokemonContext, but only use setAllPokemon funct 
-    const setPokemon = useContext(PokemonContext).setAllPokemon; 
+    // const setPokemon = useContext(PokemonContext).setAllPokemon; 
 
+    // need to consider on how it would tell the difference between searching name, type, and/or gen 
+
+    // user enters a pokemon name
     useEffect(() => {
         const doFetch = async () => {
-        const [data, error] = await handleFetch('http://localhost:4000/pokemon');
-            // need to consider on how it would tell the difference between searching name, type, and/or gen 
-            if (data) setPokemon(data.filter((pokemon) => pokemon.name.includes(nameEntry.toLowerCase())));
+            const [data, error] = await handleFetch(`https://pokeapi.co/api/v2/pokemon/${nameEntry}/`);
+            if (data) settingName(data.filter((pokemon) => pokemon.name.includes(nameEntry.toLowerCase())));
             // console.log(data);
             if (error) setError(error);
         };
         doFetch();
     }, [nameEntry]);
 
+    // user picks a pokemon type
+    useEffect(() => {
+        const doFetch = async () => {
+            const [data, error] = await handleFetch(`https://pokeapi.co/api/v2/type/${typeEntry}/`);
+            if (data) settingType(data.filter((pokemon) => pokemon.results.name.includes(typeEntry.toLowerCase())));
+            // console.log(data);
+            if (error) setError(error);
+        };
+        doFetch();
+    }, [typeEntry]);
+
+    // user picks a pokemon gen
+    useEffect(() => {
+        const doFetch = async () => {
+            const [data, error] = await handleFetch(`https://pokeapi.co/api/v2/pokemon/${genEntry}/`);
+            // need to adjust the formatting, ex: generation i !== generation-i
+            if (data) settingGen(data.filter((pokemon) => pokemon.sprite.versions.includes(genEntry.toLowerCase())));
+            // console.log(data);
+            if (error) setError(error);
+        };
+        doFetch();
+    }, [genEntry]);
+
     if (error) return <p>{error.message}</p>
 
     return (
+        // delete the <br> tags later, when doing CSS 
         <>
         <p>Hello!</p>
         <div className="ui search">
             <h1>Pokemon Selections</h1>
             <form>
                 <div className="name-search-container">
+                    <h3>Enter A Pokemon Name Here:</h3>
                     <input className="prompt" placeholder="Search by Name..." value={nameEntry} onChange={(e) => settingName(e.target.value)} />
                 </div>
                 <div className="type-or-gen-container">
                     {/* plan: these should be drop-downs, or radio buttons otherwise  */}
-                    <input className="prompt" placeholder="-" value={typeEntry} onChange={(e) => settingType(e.target.value)} />
+                    <h3>Choose A Pokemon Type Here:</h3>
+                    <select name="types" id="types">
+                        <option value="-">-</option>
+                        <option value="fire">fire</option>
+                        <option value="water">water</option>
+                        <option value="ground">ground</option>
+                        <option value="psychic">psychic</option>
+                        {/* adding more later */}
+                    </select>
+                    {/* <input className="prompt" placeholder="-" value={typeEntry} onChange={(e) => settingType(e.target.value)} /> */}
+                    <br />
+
+                    <h3>Choose A Pokemon Generation Here:</h3>
+                    {/* do the same like type */}
                     <input className="prompt" placeholder="-" value={genEntry} onChange={(e) => settingGen(e.target.value)} />
                 </div>
             </form>
         </div>
+        <br />
         <button onClick={clickPageHandler}>Go to Select Items</button>
         </>
     )
