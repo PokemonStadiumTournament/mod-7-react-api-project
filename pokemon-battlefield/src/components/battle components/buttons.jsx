b51462ac08704b32c6cf0aebe9b4e7672e39bca6
EngelsGB <engelsgarciabello@gmail.com>
2024-08-27 13:33:55 -0400

    Some fixes

--- pokemon-battlefield/src/components/battle components/buttons.jsx
+++ pokemon-battlefield/src/components/battle components/buttons.jsx
@@ -1,10 +1,38 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ActionButtons from "./action-buttons";
 import MovesButtons from "./moves-buttons";
+import PokemonContext from "../../context/PokemonContext";
 
 const BattleButtons = () => {
+    const navigate = useNavigate();
     const [currMenu, setMenu] = useState('action');
+    const playerData = useContext(PokemonContext).playerPokemon;
+    const playerHealth = useContext(PokemonContext).playerHealth;
+    const oppData = useContext(PokemonContext).oppPokemon;
+    const oppHealth = useContext(PokemonContext).oppHealth;
+    const setLog = useContext(PokemonContext).setLog;
+    const setWinner = useContext(PokemonContext).setWinner;
+    const setWinnerPokemon = useContext(PokemonContext).setWinnerPokemon;
+
+    if (oppHealth <= 0) {
+        setLog(`${oppData.name} has fainted`);
+        setWinner('The Player');
+        setWinnerPokemon(playerData);
+        setTimeout(() => {
+            navigate('/result');
+        }, 500);
+    };
+
+    if (playerHealth <= 0) {
+        setLog(`${playerData.name} has fainted`);
+        setWinner('The CPU');
+        setWinnerPokemon(oppData);
+        setTimeout(() => {
+            navigate('/result');
+        }, 500);
+    };
+
     if (currMenu === 'action') return <ActionButtons setter={setMenu} />;
     else if (currMenu === 'moves') return <MovesButtons setMenu={setMenu} />;
     else return (<></>);
--- pokemon-battlefield/src/components/battle components/moves-buttons.jsx
+++ pokemon-battlefield/src/components/battle components/moves-buttons.jsx
@@ -1,12 +1,9 @@
 import { useContext, useEffect, useState } from "react";
 import handleFetch from "../../utils/handleFetch";
 import PokemonContext from "../../context/PokemonContext";
-import { useNavigate } from "react-router-dom";
 
 const MovesButtons = ({ setMenu }) => {
-    const navigate = useNavigate();
     const [oppMove, setOppMove] = useState();
-    const playerData = useContext(PokemonContext).playerPokemon;
     const playerHealth = useContext(PokemonContext).playerHealth;
     const setPlayerHealth = useContext(PokemonContext).setPlayerHealth;
     const oppData = useContext(PokemonContext).oppPokemon;
@@ -14,8 +11,6 @@ const MovesButtons = ({ setMenu }) => {
     const setOppHealth = useContext(PokemonContext).setOppHealth;
     const moves = useContext(PokemonContext).playerMoves;
     const setLog = useContext(PokemonContext).setLog;
-    const setWinner = useContext(PokemonContext).setWinner;
-    const setWinnerPokemon = useContext(PokemonContext).setWinnerPokemon;
 
     useEffect(() => {
         const fetchOppMove = async() => {
@@ -29,32 +24,17 @@ const MovesButtons = ({ setMenu }) => {
     const handleClick = (e) => {
         const name = e.target.textContent;
         const move = moves.find((e) => e.name === name);
+        console.log(typeof oppHealth)
         setMenu('none');
         setTimeout(() => {
             setLog(`Player uses ${move.name}`);
-            setOppHealth(oppHealth - (move.power / 2));
-            if (oppHealth <= 0) {
-                setOppHealth(0);
-                setLog(`${oppData.name} has fainted`);
-                setWinner('The Player');
-                setWinnerPokemon(playerData);
-                setTimeout(() => {
-                    navigate('/result');
-                }, 500);
-            };
+            const newOppHealth = oppHealth - (move.power / 2);
+            setOppHealth(newOppHealth);
         }, 500)
         setTimeout(() => {
             setLog(`Computer uses ${oppMove.name}`);
-            setPlayerHealth(playerHealth - (oppMove.power / 2));
-            if (playerHealth <= 0) {
-                setPlayerHealth(0);
-                setLog(`${playerData.name} has fainted`);
-                setWinner('The CPU');
-                setWinnerPokemon(oppData);
-                setTimeout(() => {
-                    navigate('/result');
-                }, 500);
-            };
+            const newPlayerHealth = playerHealth - (oppMove.power / 2);
+            setPlayerHealth(newPlayerHealth);
         }, 1000)
         setTimeout(() => {
             setLog('Waiting for commands...');
